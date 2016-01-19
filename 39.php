3bc9b2b46a26907b48d12fb5cc3865d9b1da07b4
daniyaljavani <daniyal.javani@gmail.com>
2016-01-19 20:53:25 +0330

    Add 39

---
+++ 39.php
@@ -0,0 +1,17 @@
+<?php
+$times = microtime(true);
+
+for ($i = 1; $i < 500; ++$i) {
+    for ($j = $i; $j  < 500; ++$j) {
+        $k = sqrt(pow($i, 2) + pow($j, 2));
+        if ( ctype_digit((string)$k)) {
+            if ($i + $j + $k < 1000) {
+                @++$p[$i + $j + $k ];
+            }
+        }
+    }
+}
+echo microtime(true) - $times."\n";
+
+$ans = array_keys($p, max($p));
+echo $ans[0];
