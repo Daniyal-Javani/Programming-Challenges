a0b5f8ccbb5025f7a887a6c84ad9d64e0f846796
Daniyal Javani <daniyal.javani@gmail.com>
2019-03-26 16:18:39 +0430

    Add 2D Array - DS solution

---
+++ 2d-array.php
@@ -0,0 +1,39 @@
+<?php
+ // https://www.hackerrank.com/challenges/2d-array/problem
+
+function hourglassSum($arr) {
+    $max = -63;
+    for ($i = 0; $i < 4; $i++) {
+        for ($j = 0; $j < 4; $j++) {
+            $sum = $arr[$i][$j] +
+                $arr[$i][$j + 1] +
+                $arr[$i][$j + 2] +
+                $arr[$i + 1][$j + 1] +
+                $arr[$i + 2][$j] +
+                $arr[$i + 2][$j + 1] +
+                $arr[$i + 2][$j + 2];
+                
+            $max = max($sum, $max);
+        }
+    }
+
+    return $max;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+$arr = array();
+
+for ($i = 0; $i < 6; $i++) {
+    fscanf($stdin, "%[^\n]", $arr_temp);
+    $arr[] = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+}
+
+$result = hourglassSum($arr);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);
